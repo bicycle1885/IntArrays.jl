cf26da93b296420f1b661998f7eb32a02c1a9a57
Kenta Sato <bicycle1885@gmail.com>
2015-08-12 18:17:22 +0900

    optimize

--- src/buffer.jl
+++ src/buffer.jl
@@ -10,36 +10,40 @@ end
 # word size
 const W = 64
 
-function getindex{w}(buf::Buffer{w}, i::Integer)
+@inline function getindex{w}(buf::Buffer{w}, i::Integer)
     data = buf.data
-    k, r = divrem((i - 1) * w, W)
+    k, r = divrem64((i - 1) * w)
     if r + w ≤ W
-        chunk = (data[k+1] >> (W - (r + w))) & rmask(w)
+        @inbounds chunk = (data[k+1] >> (W - (r + w))) & rmask(w)
     else
-        left = (data[k+1] & rmask(W - r)) << ((r + w) - W)
-        right = data[k+2] >> (2W - (r + w))
+        @inbounds left = (data[k+1] & rmask(W - r)) << ((r + w) - W)
+        @inbounds right = data[k+2] >> (2W - (r + w))
         chunk = left | right
     end
     return chunk
 end
 
-function setindex!{w}(buf::Buffer{w}, x::UInt64, i::Integer)
+@inline function setindex!{w}(buf::Buffer{w}, x::UInt64, i::Integer)
     x &= rmask(w)
     data = buf.data
-    k, r = divrem((i - 1) * w, W)
+    k, r = divrem64((i - 1) * w)
     if r + w ≤ W
-        data[k+1] = (data[k+1] & ~mask(r, w)) | (x << (W - (r + w)))
+        @inbounds data[k+1] = (data[k+1] & ~mask(r, w)) | (x << (W - (r + w)))
     else
-        data[k+1] = (data[k+1] & ~rmask(W - r)) | (x >>> ((r + w) - W))
-        data[k+2] = (data[k+2] & ~mask(0, (r + w) - W)) | (x << (2W - (r + w)))
+        @inbounds data[k+1] = (data[k+1] & ~rmask(W - r)) | (x >>> ((r + w) - W))
+        @inbounds data[k+2] = (data[k+2] & ~mask(0, (r + w) - W)) | (x << (2W - (r + w)))
     end
     return x
 end
 
-function rmask(w)
+@inline function rmask(w)
     ~UInt64(0) >> (W - w)
 end
 
-function mask(r, w)
+@inline function mask(r, w)
     rmask(w) << (W - (r + w))
+end
+
+@inline function divrem64(n::Integer)
+    return (n >> 6, n & 0b111111)
 end
