7b1fa23b10d3dca67322b90b8c97f7b55a29a38f
Kenta Sato <bicycle1885@gmail.com>
2015-08-14 11:45:51 +0900

    update benchmark script

--- benchmark/bench.jl
+++ benchmark/bench.jl
@@ -1,23 +1,20 @@
 using IntArrays
 
-srand(12345)
-
 function bench_getindex(array, n)
-    # warming up
+    s = zero(eltype(array))
     for i in 1:endof(array)
-        array[i]
+        s += array[i]
     end
     t = @elapsed for _ in 1:n
         for i in 1:endof(array)
-            array[i]
+            s += array[i]
         end
     end
     return t / (n * length(array))
 end
 
 function bench_setindex(array, n)
-    # warming up
-    x = 0x00
+    x = rand(0x00:0x01)
     for i in 1:endof(array)
         array[i] = x
     end
@@ -45,32 +42,61 @@ function bench_fill1(array, n)
     return t / n
 end
 
+function bench_copy(array, n)
+    copy(array)
+    t = @elapsed for _ in 1:n
+        copy(array)
+    end
+    return t / n
+end
+
+function bench_copysort(array, n)
+    tmp = similar(array)
+    sort!(copy!(tmp, array))
+    t = @elapsed for _ in 1:n
+        sort!(copy!(tmp, array))
+    end
+    return t / n
+end
+
 let
+    srand(12345)
     baseline = !isempty(ARGS) && shift!(ARGS) == "--baseline"
     Ts = [UInt8, UInt16, UInt32, UInt64]
     size = 100_000
-    n = 100
-    columns = ["type", "eltype", "w", "bench_getindex", "bench_setindex", "bench_fill0", "bench_fill1"]
+    small = 5
+    large = 100
+    benchmarks = [
+        (:bench_getindex, large),
+        (:bench_setindex, large),
+        (:bench_fill0, small),
+        (:bench_fill1, small),
+        (:bench_copy, small),
+        (:bench_copysort, small),
+    ]
     if baseline
-        filter!(x -> x != "w", columns)
-        println(join(columns, '\t'))
+        columns = ["type", "eltype"]
+        println(join(vcat(columns, [name for (name, _) in benchmarks]), '\t'))
         for T in Ts
             array = Vector{T}(rand(typemin(T):typemax(T), size))
-            print(typeof(array), '\t', eltype(array), '\t')
-            print(bench_getindex(array, n), '\t')
-            print(bench_setindex(array, n), '\t')
-            print(bench_fill0(array, 10), '\t')
-            print(bench_fill1(array, 10), '\n')
+            print(typeof(array), '\t', eltype(array))
+            for (benchmark, n) in benchmarks
+                gc()
+                print('\t', eval(benchmark)(array, n))
+            end
+            println()
         end
     else
-        println(join(columns, '\t'))
+        columns = ["type", "eltype", "w"]
+        println(join(vcat(columns, [name for (name, _) in benchmarks]), '\t'))
         for T in Ts, w in 1:sizeof(T)*8
             array = IntVector{w,T}(rand(typemin(T):typemax(T), size))
-            print(typeof(array), '\t', eltype(array), '\t', w, '\t')
-            print(bench_getindex(array, n), '\t')
-            print(bench_setindex(array, n), '\t')
-            print(bench_fill0(array, 10), '\t')
-            print(bench_fill1(array, 10), '\n')
+            print(typeof(array), '\t', eltype(array), '\t', w)
+            for (benchmark, n) in benchmarks
+                gc()
+                print('\t', eval(benchmark)(array, n))
+            end
+            println()
         end
     end
 end
