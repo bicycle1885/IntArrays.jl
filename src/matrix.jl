5070aba541e66a9bdaf42f46d18e462b464e6e1c
Jim Garrison <jim@garrison.cc>
2017-08-06 08:33:44 -0400

    Fix deprecation warnings on julia 0.5 (#2)
    This is necessary for the code to run at all on julia 0.6.
    
    This also drops support for julia 0.4, which is no longer supported
    by the latest Compat.jl.

--- src/matrix.jl
+++ src/matrix.jl
@@ -1,10 +1,10 @@
 typealias IntMatrix{w,T} IntArray{w,T,2}
 
-function call{w,T}(::Type{IntMatrix{w,T}}, m::Integer, n::Integer, mmap::Bool=false)
+function (::Type{IntMatrix{w,T}}){w,T}(m::Integer, n::Integer, mmap::Bool=false)
     return IntArray{w,T}((m, n), mmap)
 end
 
-function call{w,T}(::Type{IntMatrix{w,T}}, mmap::Bool=false)
+function (::Type{IntMatrix{w,T}}){w,T}(mmap::Bool=false)
     return IntArray{w,T}((0, 0), mmap)
 end
 
